4ea8b54a434b1a917c0e87077676775b2f433f1a
tsanim <cancy@abv.bg>
2019-11-25 17:44:20 +0200

    Add Room Model

---
+++ models/Room.js
@@ -0,0 +1,11 @@
+const mongoose =  require('mongoose');
+const Schema  = mongoose.Schema;
+
+const roomSchema = new Schema({
+    pairUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
