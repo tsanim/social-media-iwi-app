c500ab8e3f121c9cfdec336cc048abfa2a606f29
tsanim <cancy@abv.bg>
2020-05-11 17:49:53 +0300

    Edit config.js

--- client/iwi-app/src/config/config.js
+++ client/iwi-app/src/config/config.js
@@ -1,6 +1,6 @@
 const heroku = 'https://iwi-app.herokuapp.com';
 const local = 'http://localhost:9999';
 
-const URI = local;
+const URI = process.env.NODE_ENV === 'development' ? local : heroku;
 
 export default URI;
