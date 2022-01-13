54025d1767751d65dba396647631ab5545f0c229
tsanim <cancy@abv.bg>
2022-01-13 18:25:17 +0200

    fix: update default user image id

--- config/config.js
+++ config/config.js
@@ -5,14 +5,14 @@ export default {
     development: {
         port: process.env.PORT || 9999,
         mongoUrl: process.env.DB_URI,
-        defaultUserImage: '5d752dd7492b893b84223fd4',
+        defaultUserImage: '61e050d36820ca4becd21d2b',
         JWT_SECRET: process.env.JWT_SECRET,
         socketPORT: 8888
     },
     production: {
         port: process.env.PORT,
         mongoUrl: process.env.PROD_MONGODB,
-        defaultUserImage: '5d752dd7492b893b84223fd4',
+        defaultUserImage: '61e050d36820ca4becd21d2b',
         JWT_SECRET: process.env.JWT_SECRET,
         socketPORT: 8888
     }
